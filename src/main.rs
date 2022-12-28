db05212a0ccd27eb44e45c0700556e7286c95b71
christofferaakre <christoffer.corfield@gmail.com>
2022-12-28 22:29:57 +0100

    working but very slow

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,14 @@ impl Cell {
             neighbour_indices: vec![],
         }
     }
+    pub fn neighbour_count(&self, cells: &Vec<Vec<Cell>>) -> u32 {
+        let mut neighbour_count = 0;
+        for &position in &self.neighbour_indices {
+            neighbour_count += cells[position.0][position.1].alive as u32;
+        }
+
+        neighbour_count
+    }
 }
 
 #[derive(Debug)]
@@ -57,6 +65,26 @@ impl Grid {
         grid
     }
 
+    pub fn advance(&mut self) {
+        let old_cells = self.cells.clone();
+        for row in self.cells.iter_mut() {
+            for cell in row {
+                match cell.neighbour_count(&old_cells) {
+                    0..=1 => {
+                        cell.alive = false;
+                    }
+                    3 => {
+                        cell.alive = true;
+                    }
+                    4.. => {
+                        cell.alive = false;
+                    }
+                    _ => {}
+                };
+            }
+        }
+    }
+
     pub fn compute_neighbour_indices(&mut self) {
         for (x, row) in self.cells.iter_mut().enumerate() {
             for (y, cell) in row.iter_mut().enumerate() {
@@ -90,6 +118,17 @@ impl Grid {
         }
     }
 
+    pub fn load_object(&mut self, object: &Object, offset: (usize, usize)) {
+        for (x, y) in &object.pixels {
+            self.cells
+                .get_mut(*x + offset.0)
+                .unwrap()
+                .get_mut(*y + offset.1)
+                .unwrap()
+                .alive = true;
+        }
+    }
+
     pub fn draw(&self) -> RgbImage {
         let mut image = RgbImage::new(self.width, self.height);
         for (x, row) in self.cells.iter().enumerate() {
@@ -113,25 +152,62 @@ const HEIGHT: u32 = 16;
 const TARGET_WIDTH: u32 = 512;
 const TARGET_HEIGHT: u32 = 512;
 
+pub struct Object {
+    pub pixels: Vec<(usize, usize)>,
+}
+
+impl Object {
+    pub fn from_file(filename: &str) -> Self {
+        let file_contents = std::fs::read_to_string(filename).expect("Failed to read file");
+        Self::from_string(file_contents)
+    }
+
+    fn from_string(buffer: String) -> Object {
+        let mut pixels = vec![];
+        let buffer = buffer.replace("(", "");
+        let buffer = buffer.replace(")", "");
+        let coords = buffer.split_whitespace();
+        for coord in coords {
+            let mut coord_vals = coord.split(",");
+            let x: usize = coord_vals
+                .next()
+                .expect("Failed to get next value in comma split")
+                .parse()
+                .expect("Failed to parse coordinate to usize");
+            let y: usize = coord_vals
+                .next()
+                .expect("Failed to get next value in comma split")
+                .parse()
+                .expect("Failed to parse coordinate to usize");
+            pixels.push((x, y));
+        }
+        Self { pixels }
+    }
+}
+
 fn main() {
-    let mut grid = Grid::new(WIDTH, HEIGHT);
-    println!("{:?}", grid);
+    let mut grid = Grid::new(16, 16);
 
-    grid.cells[0][0].alive = true;
-    let image = grid.draw();
+    let glider = Object::from_file("data/glider.life");
+    grid.load_object(&glider, (0, 0));
 
-    let filename = "out/out.png";
+    for generation in 0..10 {
+        println!("Generation {}", generation);
+        let filename = format!("out/generation-{}.png", generation);
 
-    let image = image::imageops::resize(
-        &image,
-        TARGET_WIDTH,
-        TARGET_HEIGHT,
-        image::imageops::FilterType::Nearest,
-    );
+        let image = grid.draw();
+        let image = image::imageops::resize(
+            &image,
+            TARGET_WIDTH,
+            TARGET_HEIGHT,
+            image::imageops::FilterType::Nearest,
+        );
+        grid.advance();
+        image.save(filename).expect("Failed to save file");
+    }
 
-    image.save(filename).expect("Failed to save file");
-    std::process::Command::new("sxiv")
-        .arg(filename)
-        .output()
-        .expect("Failed to execute sxiv");
+    // std::process::Command::new("sxiv")
+    //     .arg(filename)
+    //     .output()
+    //     .expect("Failed to execute sxiv");
 }
